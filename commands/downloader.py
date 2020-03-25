f0e96e7fd1129fb1540e81098ad79795630df9ce
mdgaz <mdgaziurrahmannoor@gmail.com>
2020-03-25 16:42:57 +0600

    Add Speed Indicator

--- commands/downloader.py
+++ commands/downloader.py
@@ -5,27 +5,32 @@ import shutil
 
 chunk_size = 1024
 
-def autoconvert(length):
-    length=float(length)
-    s=round(length/1048576,2)
-    if s<1024:
-        return str(s)+" MB"
-    elif s>=1024:
-        return str(round(s/1024,2))+" GB"
-    elif s>=1024*1024:
-        return str(round(s/(1024*1024),2))+" TB"
-    
+
+
+class SNode:
+    def __init__(self,size):
+        self.size,self.unit=self.autoconvert(size)
+        self.size=float(self.size)
+    def autoconvert(self,length):
+        length = float(length)
+        s = length / 1048576
+        if s < 1024:
+            return ["%.2f" % (s),"MB"]
+        elif s >= 1024:
+            return ["%.2f" % (s / 1024),"GB"]
+        elif s >= 1024 * 1024:
+            return ["%.2f" % (4 * 1024),"TB"]
 
 def Download():
-    console_columns,console_rows=shutil.get_terminal_size()
-    scls=(25*console_columns)//120
-    url=input("Enter the url for downloading file: ")
-    filename=url.split('/')[-1]
+    console_columns, console_rows = shutil.get_terminal_size()
+    scls = (25 * console_columns) // 120
+    url = input("Enter the url for downloading file: ")
+    filename = url.split('/')[-1]
     if "?" in filename:
-        filename=filename.split('?')
-        filename=filename[0]
+        filename = filename.split('?')
+        filename = filename[0]
     try:
-        response=requests.get(url,stream=True)
+        response = requests.get(url, stream=True,allow_redirects=True)
     except ConnectionError:
         print("Cannot connect to the destination url!")
         return 1
@@ -35,29 +40,34 @@ def Download():
     except:
         print("Cannot connect to the server!")
         return 1
-    dest=input("Enter the destination(with \ at the end): ")
-    file_length=response.headers['content-length']
-    curdown=0
+    dest = input("Enter the destination(with \\ at the end): ")
+    file_length = response.headers['content-length']
+    curdown = 0
     try:
-        with open(dest+filename,"wb") as f:
-            if file_length==0:
+        with open(dest + filename, "wb") as f:
+            if file_length == 0:
                 f.write(response.content)
             else:
+                start = time.perf_counter()
+                print("Downloading ",filename)
                 for data in response.iter_content(chunk_size=chunk_size):
-                    console_columns,console_rows=shutil.get_terminal_size()
-                    scls=(25*console_columns)//120
-                    curdown+=len(data)
-                    percent=(curdown*50)/(float(file_length))
+                    console_columns, console_rows = shutil.get_terminal_size()
+                    scls = (50 * console_columns) // 120
+                    curdown += len(data)
+                    percent = (curdown * 50) / (float(file_length))
                     f.write(data)
-                    barcount=int((percent*scls)//50)
-                    spacecount=scls-barcount
-                    ac=autoconvert(curdown)
-                    af=autoconvert(file_length)
-                    print("\rDownloading",filename,"[","█"*barcount," "*spacecount+"]",ac,"/",af,round((100*percent)/50,2),"%",file=sys.stdout,end='')
+                    barcount = int((percent * scls) // 50)
+                    spacecount = scls - barcount
+                    ac=SNode(curdown)
+                    af=SNode(file_length)
+                    speed=SpeedNode(ac.size/(time.perf_counter()-start))
+                    print("\r[", "█" * barcount, " " * spacecount + "]", str(ac.size)+ac.unit+"/"+str(af.size)+af.unit,"%.2f" % ((100 * percent) / 50)+"%","%.2f"%(speed.speed)+speed.unit,file=sys.stdout,end='')
                     sys.stdout.flush()
     except PermissionError:
-        print("Cannot create file in %s! Do you have the permission to do this?"%(dest))
+        print("Cannot create file in %s! Do you have the permission to do this?" % (dest))
     print()
     print("Download Completed!")
+
+
 def downloader(temp):
     Download()
